83abd0682393355ef5d180ead847cd3e06adc6c3
yang_Nick <274578405@qq.com>
2016-07-21 22:40:01 +0800

    432eqeqw

--- ylb_privileges/src/main/java/com/lesaas/mapper/RoleMapper.java
+++ ylb_privileges/src/main/java/com/lesaas/mapper/RoleMapper.java
@@ -1,5 +1,23 @@
 package com.lesaas.mapper;
 
+import java.util.List;
+
+import com.lesaas.model.Role;
+
 
 public interface RoleMapper {
+
+	List<Role> listAllRoles();
+	
+	Role getRoleById(int roleId);
+	
+	void insertRole(Role role);
+	
+	void updateRoleBaseInfo(Role role);
+	
+	void deleteRoleById(int roleId);
+	
+	int getCountByName(Role role);
+	
+	void updateRoleRights(Role role);
 }
--- ylb_privileges/src/main/java/com/lesaas/service/RoleService.java
+++ ylb_privileges/src/main/java/com/lesaas/service/RoleService.java
@@ -1,10 +1,13 @@
 package com.lesaas.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lesaas.mapper.RoleMapper;
+import com.lesaas.model.Role;
 
 
 
@@ -20,5 +23,40 @@ public class RoleService {
 	public void setRoleMapper(RoleMapper roleMapper) {
 		this.roleMapper = roleMapper;
 	}
+
+	public List<Role> listAllRoles() {
+		return roleMapper.listAllRoles();
+	}
+	
+	public void deleteRoleById(int roleId) {
+		roleMapper.deleteRoleById(roleId);
+	}
+
+	public Role getRoleById(int roleId) {
+		return roleMapper.getRoleById(roleId);
+	}
+
+	public boolean insertRole(Role role) {
+		if(roleMapper.getCountByName(role)>0)
+			return false;
+		else{
+			roleMapper.insertRole(role);
+			return true;
+		}
+	}
+
+	public boolean updateRoleBaseInfo(Role role) {
+		if(roleMapper.getCountByName(role)>0)
+			return false;
+		else{
+			roleMapper.updateRoleBaseInfo(role);
+			return true;
+		}
+	}
+	
+	public void updateRoleRights(Role role) {
+		roleMapper.updateRoleRights(role);
+	}
+
 	
 }
